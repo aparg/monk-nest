b955ed11e1dd5f3171bb52fae6c4449100690a2a
aparg <apargtm@gmail.com>
2024-07-16 22:04:59 +0545

    fix: deployment fix

--- api/routes/index.js
+++ api/routes/index.js
@@ -3,4 +3,4 @@ import { resenditialAndCondo, commercial } from "./residential";
 import { latLong } from "./latLong";
 
 export { resenditialAndCondo as resenditial, latLong };
-export { commercial as commercial, latLong };
+export { commercial as commercial };
