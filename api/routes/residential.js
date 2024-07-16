449c71ca22a52369ca3c323597ea14757cb77ea1
aparg <apargtm@gmail.com>
2024-07-16 21:59:09 +0545

    fix: deployment fix

--- api/routes/residential.js
+++ api/routes/residential.js
@@ -3,4 +3,9 @@ import { BASE_URL } from "../index";
 export const resenditialAndCondo = {
   properties: `${BASE_URL}/residential/Properties/$query`,
   photos: `${BASE_URL}/residentialPhotos/MLS-index.jpeg`,
-}
+};
+
+export const commercial = {
+  properties: `${BASE_URL}/commercial/Properties/$query`,
+  photos: `${BASE_URL}/commercialPhotos/MLS-index.jpeg`,
+};
