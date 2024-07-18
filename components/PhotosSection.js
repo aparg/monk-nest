591089034bddd4ca2197e1f1ece39ba8190c02cc
aparg <apargtm@gmail.com>
2024-07-18 18:54:40 +0545

    style: image section  and footer

--- components/PhotosSection.js
+++ components/PhotosSection.js
@@ -5,7 +5,10 @@ const PhotosSection = () => {
     <div className="flex flex-row mx-auto justify-center">
       <div className="">
         <img src="/image-section/1.png" className="w-[500px] mb-5"></img>
-        <img src="/image-section/2.png" className="w-[500px]"></img>
+        <img
+          src="/image-section/2.png"
+          className="w-[300px] sm:w-[500px]"
+        ></img>
       </div>
       <div>
         <img src="/image-section/arrow.png"></img>
