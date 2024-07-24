72ac2b4d367299bf1ac0c72d9611ce64c4d995c8
aparg <apargtm@gmail.com>
2024-07-24 16:31:13 +0545

    fix: signin redirect url

--- app/(auth)/(routes)/sign-in/[[...sign-in]]/page.js
+++ app/(auth)/(routes)/sign-in/[[...sign-in]]/page.js
@@ -1,4 +1,4 @@
 import { SignIn } from "@clerk/nextjs";
-export default function Page(){
-    return <SignIn afterSignInUrl="/after-sign-in"/>
+export default function Page() {
+  return <SignIn forceRedirectUrl="/after-sign-in" />;
 }
